f556a605deacc7ab13b8dd676ff9eadf1c761f7b
李小怪 <1695688820@qq.com>
2016-04-13 12:28:53 +0800

    TomDog is a web Server
    TomDog is a web Server, made by java

---
+++ src/test/file_list.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.io.File;
+
+/*
+ * 用来测试File类的相关函数
+ */
+
+public class file_list {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		File f = new File("G:/android/JDK");
+//		File[] files = f.listFiles();
+//		for (File file : files) {
+//			if(file.isDirectory()){
+//				System.out.println("Dir:"+file.getName()+"/");
+//			}else{
+//				System.out.println("file:"+file.getName());
+//			}
+//		}
+//		System.out.println(f.getParent());
+////		String[] fs = f.list();
+////		for (String name : fs) {
+////			System.out.println(name);
+////		}
+//		
+//	}
+
+}
